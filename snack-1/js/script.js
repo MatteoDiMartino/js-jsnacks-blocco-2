1e39f67017abe4d7dc3f666fac21937798dbd42a
MatteoDiMartino <matteodimartino359@gmail.com>
2024-03-12 15:15:27 +0100

    feat: add request num

---
+++ snack-1/js/script.js
@@ -0,0 +1,10 @@
+// - L'utente inserisce un numero nel prompt, se è pari stampa il numero,
+//   se è dispari stampa il numero successivo
+
+// chiedo di inserire un numero
+// verifico che numero è
+//  se è pari stampo
+// altrimenti se è dispari richiedo il numero che se sarà pari stampo
+
+let userNumber = parseInt(prompt('Inserisci un numero'));
+console.log (userNumber);

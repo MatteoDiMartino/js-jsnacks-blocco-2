b01d0b7d3e9fa3b54b641c39de45d5fbbd42df53
MatteoDiMartino <matteodimartino359@gmail.com>
2024-03-12 19:03:12 +0100

    fix: remove div html and add div with js

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -2,3 +2,8 @@
 // un div avrà il testo colorato di rosso mentre l’altro di verde.
 // Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
 
+const divRed = document.createElement("div");
+console.log (divRed);
+
+const divGreen = document.createElement("div");
+console.log (divGreen);
